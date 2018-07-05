0f23f06ecd028612627ce6917762d4d1b2f05327
Neftalí Michelet <neftalimich@gmail.com>
2018-07-05 00:56:33 -0500

    AmmoCapacities

---
+++ src/app/models/ammo-capacities.model.ts
@@ -0,0 +1,25 @@
+export class AmmoCapacitiesModel {
+	id: number;
+	normal?: number[];
+	piercing?: number[];
+	spread?: number[];
+	sticky?: number[];
+	cluster?: number[];
+	recover?: number[];
+	poison?: number[];
+	paralysis?: number[];
+	sleep?: number[];
+	exhaust?: number[];
+	flaming?: number;
+	water?: number;
+	freeze?: number;
+	thunder?: number;
+	dragon?: number;
+	slicing?: number;
+	wyvern?: number;
+	demon?: number;
+	armor?: number;
+	tranq?: number;
+	deviation: string;
+	specialAmmo: string;
+}
